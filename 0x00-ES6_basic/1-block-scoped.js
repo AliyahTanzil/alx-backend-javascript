295a66489b0b963cb7c6843c36c8b778ec6a6e8a
AliyahTanzil <sesaymohamdaugustin@gmail.com>
2023-06-27 18:03:51 +0000

    Update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,17 +1,10 @@
 // Declare and export a default function named "taskBlock" that takes a parameter "trueOrFalse"
 export default function taskBlock(trueOrFalse) {
-    // Declare a variable "task" and assign it the value false
-    var task = false;
-    // Declare a variable "task2" and assign it the value true
-    var task2 = true;
-    // Check the value of "trueOrFalse" in the conditional block
-    if (trueOrFalse) {
-      // Declare a new variable "task" inside the conditional block (not overwriting the outer "task" variable) and assign it the value true
-      let task = true;
-      // Declare a new variable "task2" inside the conditional block (not overwriting the outer "task2" variable) and assign it the value false
-      let task2 = false;
-    }
-    // Return an array with the values of the "task" and "task2" variables
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
   }
-  
+  return [task, task2];
+}
