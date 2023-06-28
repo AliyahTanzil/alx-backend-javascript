6bc4161bd10ac08193d3b71d5c45f14e3e5e1e9e
AliyahTanzil <sesaymohamdaugustin@gmail.com>
2023-06-28 00:33:43 +0000

    2 then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,7 @@
-function handleResponseFromAPI(promise) {
-    return promise
-      .then((response) => {
-        console.log('Got a response from the API');
-        return {
-          status: 200,
-          body: 'success'
-        };
-      })
-      .catch((error) => {
-        console.log('Got a response from the API');
-        return new Error();
-      });
-  }
-  
+  /* eslint-disable no-console */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
