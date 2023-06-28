497c9f2efb90e7a5d3c66bf12aca1ab0d6210b22
AliyahTanzil <sesaymohamdaugustin@gmail.com>
2023-06-28 01:27:24 +0000

    100-wait

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,29 @@
+// Import the necessary functions from utils.js
+const { uploadPhoto, createUser } = require('./utils');
+
+// Define the asyncUploadUser function
+async function asyncUploadUser() {
+  try {
+    // Call the uploadPhoto and createUser functions asynchronously
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+
+    // Await the resolutions of both promises
+    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+
+    // Return an object with the responses
+    return { photo: photoResponse, user: userResponse };
+  } catch (error) {
+    console.error('Error uploading user:', error);
+    return {}; // Return an empty object if there was an error
+  }
+}
+
+// Call the asyncUploadUser function
+asyncUploadUser()
+  .then((result) => {
+    console.log('Upload user result:', result);
+  })
+  .catch((error) => {
+    console.error('Error in asyncUploadUser:', error);
+  });
